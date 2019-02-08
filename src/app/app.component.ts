b9bc9a020ad07c378d8ccc905e3553125c5e699d
sonuammy <amsusingh990@gmail.com>
2019-02-09 00:52:24 +0530

    changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   message: string = 'Sharing Data from Parent to Child Component';
+  phonenumber: number = 8073786788;
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent {
+
+  @Input() x:number;
+
+}
